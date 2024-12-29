e0a98b90204127af5ca20c0bebfcb76605302a43
Andy Hoffman <antibland@gmail.com>
2024-12-29 16:51:41 -0500

    chore: fix build issues with path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/personal-website-with-agent3/",
+  base: "/vite-personal-site/",
   plugins: [
     {
       enforce: "pre",
