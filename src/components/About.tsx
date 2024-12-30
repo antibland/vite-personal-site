42241599ad350c11074d95d7f9260d686890640d
Andy Hoffman <antibland@gmail.com>
2024-12-30 02:22:23 -0500

    chore: add linkedin to About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -31,21 +31,21 @@ const About: FC = () => {
               contentVisibility: "auto",
             }}
           />
-          <h1>Hey, I'm Andy</h1>
-          <p>
-            Hi, I'm Andy Hoffman, a front-end engineer building elegant
+          <h1>👋 Internet</h1>
+          <p style={{ fontSize: "var(--text-lg)" }}>
+            Hi, I'm Andy Hoffman, a senior front-end engineer building elegant
             solutions to complex problems. Today, everyone brands themselves
             with the full-stack moniker, and while I'm technically full stack,
-            my strongest skills are on the front end of the stack.
+            my strongest skills are on the front end of the stack. I'm proud to
+            be a front-end engineer.
           </p>
-          <p>
-            I started tinkering on the web back in 2000, and I fell in love with
-            HTML semantics and the simplicity and expressiveness of clean markup
-            and CSS. Despite navigating the sometimes tangled jungles of JSX, I
-            always narrow my focus toward what matters and what does not. Once
-            everything unnecessary is removed, the front end revs and purrs like
-            a well-tuned engine. This has never changed since I started and has
-            never been more true in modern web development.
+          <p style={{ fontSize: "var(--text-lg)" }}>
+            I started tinkering on the web back in 2000, and was quickly drawn
+            to HTML semantics and the simplicity and expressiveness of clean
+            markup and CSS. Becoming skilled in UI/UX means identifying what
+            matters to users and what does not. Once everything unnecessary is
+            removed, the front end revs and purrs like a well-tuned machine.
+            This has never changed since I started and is even more true today.
           </p>
         </div>
         <aside className="about-sidebar">
@@ -112,6 +112,24 @@ const About: FC = () => {
                 />
               </svg>
               <span>Stack Overflow</span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/andybhoffman/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-link"
+            >
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                style={{ width: "24px", height: "24px" }}
+              >
+                <path
+                  fill="currentColor"
+                  d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
+                />
+              </svg>
+              <span>LinkedIn</span>
             </a>
             <a
               href={getAssetPath("assets/Andy_Hoffman_Resume.pdf")}
