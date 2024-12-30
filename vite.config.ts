c04f64a32e634218fa4b15190f75f95b7019b947
Andy Hoffman <antibland@gmail.com>
2024-12-29 22:05:33 -0500

    feat: add proper syntax highlighting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import rehypePrism from "rehype-prism-plus";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
           remarkFrontmatter,
           [remarkMdxFrontmatter, { name: "frontmatter" }],
         ],
+        rehypePlugins: [rehypePrism],
         providerImportSource: "@mdx-js/react",
       }),
     },
